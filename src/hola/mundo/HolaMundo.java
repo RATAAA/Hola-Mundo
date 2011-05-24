ab1345405adfe0ab73ce4518c11fd4216e30862b
Alex <aocamph@gmail.com>
2011-05-24 00:14:43 -0500

    Primer commit :)

---
+++ src/hola/mundo/HolaMundo.java
@@ -0,0 +1,17 @@
+package hola.mundo;
+
+public class HolaMundo {
+
+	public HolaMundo() {
+		System.out.println("Primer intento de subir un repositorio a la nube");
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
